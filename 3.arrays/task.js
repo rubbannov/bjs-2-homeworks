c8674b0f7a322b33508aaa00d5d1fb6bfcf308ae
Igor Rubanov <fomagreen@yandex.ru>
2023-07-16 18:01:15 +0300

    add homework functions

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    let diffirence = JSON.stringify(arr1) === JSON.stringify(arr2);
+    return diffirence;
+};
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    if (!users.length) {
+        result = 0;
+    } else if (!users.some(user => user.gender === gender)) {
+        result = 0;
+    } else {
+        let amount = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item) => acc + item);
+        result = amount / users.filter(user => user.gender === gender).map(user => user.age).length;
+    };
+    return result;
+};
