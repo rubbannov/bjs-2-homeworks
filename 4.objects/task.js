c6b10e7d96c852734293f5ec7dd9398e2dad5053
Igor Rubanov <fomagreen@yandex.ru>
2023-07-20 15:38:03 +0300

    reformat code

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,39 +1,39 @@
 function Student(name, gender, age) {
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
-  this.marks = [];
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if (this.hasOwnProperty("marks")) {
-    this.marks.push(...marks);
-  };
+    if (this.hasOwnProperty("marks")) {
+        this.marks.push(...marks);
+    };
 }
 
 Student.prototype.getAverage = function () {
-  if(this.hasOwnProperty("marks")){
-    if(!this.marks.length) {
-      result = 0;
+    if (this.hasOwnProperty("marks")) {
+        if (!this.marks.length) {
+            result = 0;
+        } else {
+            result = 0;
+            this.marks.forEach(element => {
+                result += element;
+            });
+            result = result / this.marks.length;
+        };
+        return result;
     } else {
-      result = 0;
-      this.marks.forEach(element => {
-          result += element;
-        });
-        result = result / this.marks.length;
+        return 0;
     };
-  return result;
-  } else {
-    return 0;
-  };
 }
 
 Student.prototype.exclude = function (reason) {
-  delete this.subject;
-  delete this.marks;
-  this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
