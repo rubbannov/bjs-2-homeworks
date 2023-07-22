e0b73cf5c6b127a5f84937a6bda279858bc69672
Igor Rubanov <fomagreen@yandex.ru>
2023-07-22 16:24:26 +0300

    add code

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿function parseCount(numOfString) {
+    let result = Number.parseFloat(numOfString);
+    if (isNaN(result)) {
+        throw new Error("Невалидное значение");
+    } else {
+        return result;
+    };
+};
+
+function validateCount(numOfString) {
+    try {
+        let result = parseCount(numOfString);
+        return result;
+    } catch (error) {
+        return error;
+    };
+};
+
+class Triangle {
+    constructor(a, b, c) {
+        this.sideA = a;
+        this.sideB = b;
+        this.sideC = c;
+        if (a + b < c || a + c < b || b + c < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        };
+    };
+    get perimeter() {
+        let perim = this.sideA + this.sideB + this.sideC;
+        return perim;
+    };
+    get area() {
+        let p = (this.sideA + this.sideB + this.sideC) / 2;
+        let area = Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC));
+        let resultArea = + area.toFixed(3)
+        return resultArea;
+    };
+};
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        let obj = {
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            },
+        };
+        return obj;
+    }
+}
